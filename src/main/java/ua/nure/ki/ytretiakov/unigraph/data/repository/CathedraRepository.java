03a340231c9e80add51ab54577747383a7bca3fd
Yaroslav Tretiakov <tretyak.yar@gmail.com>
2019-05-22 16:39:06 +0300

    Fixed last commit

--- src/main/java/ua/nure/ki/ytretiakov/unigraph/data/repository/CathedraRepository.java
+++ src/main/java/ua/nure/ki/ytretiakov/unigraph/data/repository/CathedraRepository.java
@@ -1,6 +1,7 @@
 package ua.nure.ki.ytretiakov.unigraph.data.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import ua.nure.ki.ytretiakov.unigraph.data.model.Cathedra;
 
-public interface CathedraRepository extends JpaRepository<CathedraRepository, String> {
+public interface CathedraRepository extends JpaRepository<Cathedra, String> {
 }
--- src/test/java/ua/nure/ki/ytretiakov/unigraph/data/service/UnigraphServiceTest.java
+++ src/test/java/ua/nure/ki/ytretiakov/unigraph/data/service/UnigraphServiceTest.java
@@ -1,8 +1,10 @@
 package ua.nure.ki.ytretiakov.unigraph.data.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import ua.nure.ki.ytretiakov.unigraph.WebContextLoader;
 
 public class UnigraphServiceTest extends WebContextLoader {
 
-
+    @Autowired
+    private UnigraphService service;
 }
